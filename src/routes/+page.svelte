8df124235548ed9f909000e247ecfa93275fecfc
JohannesChopov <johannes.chopov@student.uhasselt.be>
2024-05-27 17:56:12 +0200

    About page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    let user = 'Johannes';
+</script>
+
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
+
+
+
+<h1>Welcome to SvelteKit, {user}!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p> Lorem Ipsum. </p>
+
+<a href="/about">About</a>
+
+<h2> Dit is een voorbeeld website </h2>
+
+<style>
+    h1, h2 {
+        color: blue;   
+    }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<h1> About </h1>
+
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
+
+
+<a href="/">Home</a>
