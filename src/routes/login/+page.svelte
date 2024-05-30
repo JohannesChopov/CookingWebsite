fd2abe6d8fcabb9d64f8a10db81bef86542b42b2
JohannesChopov <johannes.chopov@student.uhasselt.be>
2024-05-30 23:58:35 +0200

    upload recipe to table

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,8 +1,10 @@
 <script>
     import Authenticate from "$lib/components/Authenticate.svelte";
-
+    import AddRecipe from "$lib/components/AddRecipe.svelte";
 </script>
 
 <a href="/">Home</a>
 
 <Authenticate/>
+
+<AddRecipe/>
